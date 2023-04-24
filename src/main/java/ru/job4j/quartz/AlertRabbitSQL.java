20e7c5d9a394dae8796b08946db60f75b26bc255
krugo <krugovenkov@mail.ru>
2023-04-24 10:11:04 +0300

    refactor

--- src/main/java/ru/job4j/quartz/AlertRabbitSQL.java
+++ src/main/java/ru/job4j/quartz/AlertRabbitSQL.java
@@ -6,8 +6,6 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.InputStream;
 import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
